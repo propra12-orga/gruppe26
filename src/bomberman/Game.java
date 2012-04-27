9efedde53cd33047be1ec2fe6258e07e84967f59
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2012-04-27 21:46:50 +0200

    draw squared for unbreakable walls

--- src/bomberman/Game.java
+++ src/bomberman/Game.java
@@ -7,17 +7,19 @@ import bomberman.game.character.BomberHuman;
 public class Game {
 
 	private final int TILESIZE = 50;
-	private final Board field;
+	private final Board board;
 	private final BomberHuman bman;
+	private final int height;
+	private final int width;
 
-	public Game() {
-		field = new Board(15, 30);
+	public Game(final int height, final int width) {
+		board = new Board(height, width);
 		bman = new BomberHuman(true, 0, 0);
+		this.height = height;
+		this.width = width;
 	}
 
 	public void start() {
-		final int width = field.getField()[0].length;
-		final int height = field.getField().length;
 		StdDraw.setCanvasSize(width * TILESIZE, height * TILESIZE);
 
 		StdDraw.setXscale(0, width * TILESIZE);
@@ -53,7 +55,13 @@ public class Game {
 	}
 
 	private void drawSomething() {
-		StdDraw.clear();
+		// StdDraw.clear();
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (board.getField()[j][i] != 0)
+					StdDraw.filledSquare(i * 50 + 25, j * 50 + 25, 25);
+			}
+		}
 		StdDraw.circle(bman.getPosX(), bman.getPosY(), 5);
 	}
 }
--- src/bomberman/Main.java
+++ src/bomberman/Main.java
@@ -3,7 +3,7 @@ package bomberman;
 public class Main {
 
 	public static void main(String[] args) {
-		final Game g = new Game();
+		final Game g = new Game(15, 30);
 		g.start();
 	}
 }
