0294df247985a1962de7d255afa3742a17311adf
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2012-04-27 15:03:28 +0200

    added some basic classes

---
+++ src/bomberman/game/objects/Bomb.java
@@ -0,0 +1,11 @@
+package bomberman.game.objects;
+
+public class Bomb {
+	final private int posX;
+	final private int posY;
+
+	public Bomb(final int posX, final int posY) {
+		this.posX = posX;
+		this.posY = posY;
+	}
+}
