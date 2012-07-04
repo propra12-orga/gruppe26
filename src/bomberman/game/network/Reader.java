f30bcb864b7be9ab43692d357d48d6dc7f97ebcd
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2012-07-04 12:15:03 +0200

    closes #21. huzzah!

--- src/bomberman/game/network/Reader.java
+++ src/bomberman/game/network/Reader.java
@@ -9,6 +9,23 @@ import bomberman.game.objects.Bomb;
 /**
  * Interface zur Netzwerkkommunikation
  * 
+ * <pre>
+ * Netzwerkprotokoll:
+ * Server akzeptiert Verbindung.
+ * Server schreibt das Level.toString() ins Netzwerk, gefolgt von "poison".
+ * Client liest und antwortet mit "ack".
+ * 
+ * Game loop beginnt hier.
+ * Server und Client schreiben je zwei Zeilen ins Netzwerk:
+ * 1. mv X Y
+ * 	wobei X die X- und Y die Y-Koordinate des Bomferman des jeweiligen Spielers ist.
+ * 2. bomb Z
+ * 	wobei Z entweder 0 oder 1 ist. 0 bedeutet, es wurde keine Bombe im Tick gelegt, 1 bedeutet, es wurde eine gelegt.
+ * Server und Client lesen die Informationen.
+ * Game loop endet hier.
+ * Zuletzt schreiben beide Seiten noch einmal die beiden Zeilen, wie im Game loop ins Netzwerk, um sicherzustellen, dass auch der letzte Tick synchronisiert wird.
+ * </pre>
+ * 
  * @author jessica
  * 
  */
@@ -35,8 +52,7 @@ public interface Reader {
 	/**
 	 * liest Leveldatei aus dem Netzwerk
 	 * 
-	 * @return
-	 *         gelesenes Level
+	 * @return gelesenes Level
 	 */
 	Level readLevel();
 }
